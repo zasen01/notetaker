b0307f82108130f6448a85a20d689508726f52e9
Myeisha Swain <zse0296@gmail.com>
2022-11-22 17:36:41 -0500

    created delete route

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const { application } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const {v4:uuidv4} = require("uuid");
+const readFile = util.promisify(fs.readFile);
+const getNotes = () => {
+    return readFile("db/db.json","utf-8").then(rawNotes => [].concat(JSON.parse(rawNotes)))
+    };
+
+const writeFile = util.promisify(fs.writeFile);
 
 const PORT = 3001;
 const app = express();
@@ -10,8 +17,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("public"));
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"));
+});
+//displays notes page
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/notes.html"));
+});
+
+app.get("/api/notes",(req,res)=>{
+   //get notes from db
+    getNotes().then(notes => res.json(notes))
+});
+
+app.post("/api/notes",(req,res) =>{
+    getNotes().then(oldNotes => {
+       var newArray = [...oldNotes,{title:req.body.title, text: req.body.text, id:uuidv4()}]
+       writeFile("db/db.json",JSON.stringify(newArray)).then(()=>res.json({msg:"ok"}))
+    })
+});
+app.delete("/api/notes/:id", (req,res)=>{
+    getNotes().then(unfilteredNotes =>{
+        let filteredNotes = unfilteredNotes.filter(note => note.id !== req.params.id)
+        writeFile("db/db.json",JSON.stringify(filteredNotes)).then(()=>res.json({msg:"ok"}))
+        
+    })
+});
+
 app.listen(PORT, ()=> {console.log("app Running")});
 
-//write 5 routes
-//two static to send index and notes html
 //3 api routes to get, add and delete note
