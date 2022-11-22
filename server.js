898333fcb9d32579ffdf8fac36c35a860ec33dd4
Myeisha Swain <zse0296@gmail.com>
2022-11-22 17:46:24 -0500

    fixed port issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const getNotes = () => {
 
 const writeFile = util.promisify(fs.writeFile);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
